0663447c196130c3145b7c50d71466910893b9df
Saurabh <saurabhbharade@gmail.com>
2018-12-19 11:49:32 +0530

    line 2 addedby feature 3

--- EclipseGitDemo/src/com/demo/EclipseGitDemo1.java
+++ EclipseGitDemo/src/com/demo/EclipseGitDemo1.java
@@ -4,5 +4,6 @@ public class EclipseGitDemo1 {
 public static void main(String[] args) {
 System.out.println("Added by client 1");
 System.out.println("line added by feature 3");
+System.out.println("line2 added by feature 3");
 }
 }
