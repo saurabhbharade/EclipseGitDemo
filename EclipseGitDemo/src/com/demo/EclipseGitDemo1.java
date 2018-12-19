9bf2ac561f45732fa49dcc9fa816f01d140e4604
Saurabh <saurabhbharade@gmail.com>
2018-12-19 11:45:55 +0530

    line added by feature 3

--- EclipseGitDemo/src/com/demo/EclipseGitDemo1.java
+++ EclipseGitDemo/src/com/demo/EclipseGitDemo1.java
@@ -2,6 +2,7 @@ package com.demo;
 
 public class EclipseGitDemo1 {
 public static void main(String[] args) {
-System.out.println("Added by client 1");	
+System.out.println("Added by client 1");
+System.out.println("line added by feature 3");
 }
 }
