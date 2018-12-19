eeda6129d2cc252fb8c744a0493b265dd64efbf1
Saurabh <saurabhbharade@gmail.com>
2018-12-19 10:59:03 +0530

    line 2 added by client 1

--- EclipseGitDemo/src/com/demo/EclipseGitDemo1.java
+++ EclipseGitDemo/src/com/demo/EclipseGitDemo1.java
@@ -3,5 +3,6 @@ package com.demo;
 public class EclipseGitDemo1 {
 public static void main(String[] args) {
 System.out.println("line added after resolving conflict");
+System.out.println("line 2 added by client 1");
 }
 }
